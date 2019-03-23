31ee09c530d1a143485fefd42c853fad2358a05a
AlanKimo <alan@iceberg9.com>
2019-03-23 14:13:50 -0600

    checkpoint

--- web-server-alan/index.js
+++ web-server-alan/index.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
 	const httpMethod = req.method
 	let fecha = Date.now()
+	//let fecha = '000999'
+	let bodyPost = [];
 
 	switch(httpMethod) {
 		case 'POST':
@@ -12,19 +14,25 @@ const server = http.createServer((req, res) => {
 				'Content-Type': 'application/json',
 			});
 
-			let bodyPost = [];
-
 			req.on('data', chunk => {
 				bodyPost.push(chunk)
 			});
 			req.on('end', () => {
+				//let bodyPostRender = JSON.parse(bodyPost)
 				bodyPost = Buffer.concat(bodyPost).toString();
 				let output = fs.createWriteStream(`${ fecha }-save.txt`)
 				output.write(bodyPost, 'UTF8')
 				output.end()
+
+				//return res.end(fs.readFileSync(`./${ fecha }-save.txt`))
+				//return res.end('{"response": true}')
+
+				// return res.end('{ "response": true }')
+				//return res.end('Response', JSON.parse(bodyPost))
+				return res.end(fs.readFileSync(`${ fecha }-save.txt`))
+
 			})
 
-			return res.end()
 
 			break
 	}
